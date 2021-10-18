5d8121991e8c45169e3beec89de4df0fe78f61c8
mvp-JimChang <chehsuan.chang@mvpfastlane.com>
2021-10-18 23:19:58 +0800

    prettier format

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,8 +20,7 @@ export const Home = (): JSX.Element => (
       <button
         onClick={() => {
           window.alert('With typescript and Jest')
-        }}
-      >
+        }}>
         Test Button
       </button>
 
@@ -38,16 +37,14 @@ export const Home = (): JSX.Element => (
 
         <a
           href="https://github.com/vercel/next.js/tree/master/examples"
-          className="card"
-        >
+          className="card">
           <h3>Examples &rarr;</h3>
           <p>Discover and deploy boilerplate example Next.js projects.</p>
         </a>
 
         <a
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
+          className="card">
           <h3>Deploy &rarr;</h3>
           <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
         </a>
@@ -58,8 +55,7 @@ export const Home = (): JSX.Element => (
       <a
         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
         target="_blank"
-        rel="noopener noreferrer"
-      >
+        rel="noopener noreferrer">
         Powered by{' '}
         <Image src="/vercel.svg" alt="Vercel Logo" height={'32'} width={'64'} />
       </a>
